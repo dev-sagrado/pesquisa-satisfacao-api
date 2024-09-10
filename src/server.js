c0e1f90f39cf072760cc142f1310afc91e4a8c5a
LucasVieiraS <lucasvieirasilva.lpta@gmail.com>
2024-09-09 22:37:49 -0300

    fix: add port

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
-const app = require('./app'); 
- 
- 
-app.listen(PORT, () =>
-    console.log(`Server is running on port ${PORT}`)
-); 
+require('dotenv').config();  // Carregar variáveis de ambiente do .env
+const app = require('./app');
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
